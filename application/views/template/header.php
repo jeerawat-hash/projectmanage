983a646f4de99c1561eda4ae4edff71a70d5184d
jeerawat <s6202041520016@email.kmutnb.ac.th>
2021-02-16 18:20:37 +0700

    add

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -104,8 +104,8 @@
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="#" class="brand-link">
-      <img src="https://projectmanage.webclient.me/assets/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light">ชื่อองค์กร</span>
+      <img src="https://www.eng.kmutnb.ac.th/web/wp-content/uploads/2019/01/logo_kmutnbNew-254x300.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+      <span class="brand-text font-weight-light">KM ProjectManagement</span>
     </a>
 
     <!-- Sidebar -->
@@ -113,10 +113,10 @@
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-          <img src="https://projectmanage.webclient.me/assets/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+          <img src="https://projectmanage.webclient.me/assets/dist/img/avatar4.png" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block">ชื่อผู้เข้าใช้</a>
+          <a href="#" class="d-block">จีระวัฒน์</a>
         </div>
       </div>
 
