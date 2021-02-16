a7bc2f220e48cde0d9bbea1cf7be478f58ae4137
jeerawat <s6202041520016@email.kmutnb.ac.th>
2021-02-16 23:19:35 +0700

    add

--- application/views/project.php
+++ application/views/project.php
@@ -461,7 +461,7 @@ $(".custom-file-input").on("change", function() {
         html += '<label for="inputDetailPeriodDate">วันที่</label>';
         html += '<input type="text" id="inputDetailPeriodDate" class="form-control">';
         html += '<div class="col-2">';
-        html += '<button id="removeRow" type="button" class="btn btn-danger">ลบ</button>';
+        html += '<button id="removeRow" type="button" class="btn btn-danger form-control">ลบ</button>';
         html += '</div>';
         html += '</div>';
         html += '</div>';
