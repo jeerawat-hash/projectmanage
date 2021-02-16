1e5a6106e9302481b817067845f18b383bcaccc0
jee <s6202041520016@email.kmutnb.ac.th>
2021-02-16 22:24:48 +0700

    1

--- application/views/signin.php
+++ application/views/signin.php
@@ -1,10 +1,4 @@
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.js"></script> 
-
- <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-
+ 
 <!DOCTYPE html>
 <html lang="en">
 <head>
