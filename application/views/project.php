99791c6add48133d40a024ee155af2e57d4c3b23
jeerawat <s6202041520016@email.kmutnb.ac.th>
2021-02-16 18:47:04 +0700

    add

--- application/views/project.php
+++ application/views/project.php
@@ -142,246 +142,132 @@
         </ul>
         
         <div class="tab-content mt-2">
+
+
           <div class="tab-pane fade show active" id="infoPanel" role="tabpanel">
             <h4>Information</h4>
-            <div class="form-group">
-              <label for="campaignName">Campaign Name</label>
-              <input type="text" class="form-text" id='campaignName'></input>
-            </div>
-            <button class="btn btn-secondary" id="infoContinue">Continue</button>
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            <button class="btn btn-secondary" id="infoContinue">ต่อไป</button>
           </div>
+
+
+
+
           <div class="tab-pane fade" id="ads" role="tabpanel">
             <h4>Ads</h4>
-            <div class="form-group">
-              <label for="exampleInputFile">Fullscreen Ad Image</label>
-              <input type="file" class="form-control-file" id="exampleInputFile" aria-describedby="fileHelp">
-              <small id="fileHelp" class="form-text text-muted">Select a file to use as the fullscreen ad image. Please ensure the size is at least 1080x1920 with a 9:16 (portrait) aspect ratio.</small>
-            </div>
-            <div class="form-group">
-              <label for="exampleInputFile">Banner Ad Image</label>
-              <input type="file" class="form-control-file" id="exampleInputFile" aria-describedby="fileHelp">
-              <small id="fileHelp" class="form-text text-muted">Select a file to use as the banner ad image. Please ensure the size is exactly 1080x450 for proper rendering.</small>
-            </div>
-            <button class="btn btn-secondary" id="adsContinue">Continue</button>
+             
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            <button class="btn btn-secondary" id="adsContinue">ต่อไป</button>
           </div>
+
+
+
+
           <div class="tab-pane fade" id="placementPanel" role="tabpanel">
             <h4>Placement</h4>
-            <div id="accordion" class="mb-3" role="tablist" aria-multiselectable="true">
-              <div class="card">
-                <div class="card-header" role="tab" id="headingOne">
-                  <h5 class="mb-0">
-                    <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                      Entire Venue
-                    </a>
-                  </h5>
-                </div>
+             
 
-                <div id="collapseOne" class="collapse" role="tabpanel" aria-labelledby="headingOne">
-                  <div class="card-block">
-                    <div class="form-group">
-                      <label for="venueSelect">Select a Venue</label>
-                      <select class="form-control" id="venueSelect">
-                        <option selected disabled>Choose a venue</option>
-                        <option>Venue 1</option>
-                        <option>Venue 2</option>
-                        <option>Venue 3</option>
-                        <option>Venue 4</option>
-                        <option>Venue 5</option>
-                      </select>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div class="card">
-                <div class="card-header" role="tab" id="headingTwo">
-                  <h5 class="mb-0">
-                    <a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                      Specific Kiosks
-                    </a>
-                  </h5>
-                </div>
-                <div id="collapseTwo" class="collapse" role="tabpanel" aria-labelledby="headingTwo">
-                  <div class="card-block">
-                    <div class="form-group">
-                      <label for="kioskSelectVenue">First, choose a venue.</label>
-                      <select class="form-control" id="kioskSelectVenue">
-                        <option selected disabled>Choose a venue</option>
-                        <option>Venue 1</option>
-                        <option>Venue 2</option>
-                        <option>Venue 3</option>
-                        <option>Venue 4</option>
-                        <option>Venue 5</option>
-                      </select>
-                    </div>
-                    <div class="form-group">
-                      <label for="exampleSelect2">Then select kiosks (you can select multiple)</label>
-                      <select multiple class="form-control" id="exampleSelect2">
-                        <option>Kiosk 1</option>
-                        <option>Kiosk 2</option>
-                        <option>Kiosk 3</option>
-                        <option>Kiosk 4</option>
-                        <option>Kiosk 5</option>
-                      </select>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div class="card">
-                <div class="card-header" role="tab" id="headingThree">
-                  <h5 class="mb-0">
-                    <a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                      Specific Screens
-                    </a>
-                  </h5>
-                </div>
-                <div id="collapseThree" class="collapse" role="tabpanel" aria-labelledby="headingThree">
-                  <div class="card-block">
-                    <div class="form-group">
-                      <label for="kioskSelectVenue">First, choose a venue.</label>
-                      <select class="form-control" id="kioskSelectVenue">
-                        <option selected disabled>Choose a venue</option>
-                        <option>Venue 1</option>
-                        <option>Venue 2</option>
-                        <option>Venue 3</option>
-                        <option>Venue 4</option>
-                        <option>Venue 5</option>
-                      </select>
-                    </div>
-                    <div class="form-group">
-                      <label for="exampleSelect2">Then select screens (you can select multiple)</label>
-                      <select multiple class="form-control" id="exampleSelect2">
-                        <option>Kiosk 1 - Screen 1</option>
-                        <option>Kiosk 1 - Screen 2</option>
-                        <option>Kiosk 2 - Screen 1</option>
-                        <option>Kiosk 2 - Screen 2</option>
-                      </select>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <button class="btn btn-secondary" id="placementContinue">Continue</button>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            <button class="btn btn-secondary" id="placementContinue">ต่อไป</button>
           </div>
           <div class="tab-pane fade" id="schedulePanel" role="tabpanel">
             <h4>Schedule</h4>
-            <div id="scheduleAccordion" class="mb-3" role="tablist" aria-multiselectable="true">
-              <div class="card">
-                <div class="card-header" role="tab" id="headingOne">
-                  <h5 class="mb-0">
-                    <a data-toggle="collapse" data-parent="#scheduleAccordion" href="#scheduleAccordioncollapseOne" aria-expanded="true" aria-controls="collapseOne">
-                      Start and Stop Date
-                    </a>
-                  </h5>
-                </div>
+           
 
-                <div id="scheduleAccordioncollapseOne" class="collapse" role="tabpanel" aria-labelledby="headingOne">
-                  <div class="card-block">
-                    <div class="form-group row">
-                      <label for="example-date-input" class="col-2 col-form-label">Start Date</label>
-                      <div class="col-10">
-                        <input class="form-control" type="date" value="2018-01-09" id="start-date">
-                      </div>
-                    </div>
-                    <div class="form-group row">
-                      <label for="example-date-input" class="col-2 col-form-label">Stop Date</label>
-                      <div class="col-10">
-                        <input class="form-control" type="date" value="2018-01-09" id="stop-date">
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div class="card">
-                <div class="card-header" role="tab" id="headingTwo">
-                  <h5 class="mb-0">
-                    <a class="collapsed" data-toggle="collapse" data-parent="#scheduleAccordion" href="#scheduleAccordioncollapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                      Rules for Specific Days
-                    </a>
-                  </h5>
-                </div>
-                <div id="scheduleAccordioncollapseTwo" class="collapse" role="tabpanel" aria-labelledby="headingTwo">
-                  <div class="card-block">
-                    <h6>Play on the following days (check all that apply)</h6>
-                    <div class="form-check">
-                      <label class="form-check-label">
-                        <input type="checkbox" class="form-check-input" id="sunday">
-                        Sunday
-                      </label>
-                    </div>
-                    <div class="form-check">
-                      <label class="form-check-label">
-                        <input type="checkbox" class="form-check-input" id="monday">
-                        Monday
-                      </label>
-                    </div>
-                    <div class="form-check">
-                      <label class="form-check-label">
-                        <input type="checkbox" class="form-check-input" id="tuesday">
-                        Tuesday
-                      </label>
-                    </div>
-                    <div class="form-check">
-                      <label class="form-check-label">
-                        <input type="checkbox" class="form-check-input" id="wednesday">
-                        Wednesday
-                      </label>
-                    </div>
-                    <div class="form-check">
-                      <label class="form-check-label">
-                        <input type="checkbox" class="form-check-input" id="thursday">
-                        Thursday
-                      </label>
-                    </div>
-                    <div class="form-check">
-                      <label class="form-check-label">
-                        <input type="checkbox" class="form-check-input" id="friday">
-                        Friday
-                      </label>
-                    </div>
-                    <div class="form-check">
-                      <label class="form-check-label">
-                        <input type="checkbox" class="form-check-input" id="saturday">
-                        Saturday
-                      </label>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div class="card">
-                <div class="card-header" role="tab" id="headingThree">
-                  <h5 class="mb-0">
-                    <a class="collapsed" data-toggle="collapse" data-parent="#scheduleAccordion" href="#scheduleAccordioncollapseThree" aria-expanded="false" aria-controls="collapseThree">
-                      Rules for Specific Times
-                    </a>
-                  </h5>
-                </div>
-                <div id="scheduleAccordioncollapseThree" class="collapse" role="tabpanel" aria-labelledby="headingThree">
-                  <div class="card-block">
-                    <h6>Play during the following timeframes (applies to each day)</h6>
-                    <div class="form-group row">
-                      <label for="example-time-input" class="col-2 col-form-label">Start Time</label>
-                      <div class="col-10">
-                        <input class="form-control" type="time" value="13:45:00" id="start-time">
-                      </div>
-                    </div>
-                    <div class="form-group row">
-                      <label for="example-time-input" class="col-2 col-form-label">End Time</label>
-                      <div class="col-10">
-                        <input class="form-control" type="time" value="13:45:00" id="end-time">
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <button class="btn btn-secondary" id="scheduleContinue">Continue</button>
+
+
+
+
+
+
+
+
+
+
+
+
+            <button class="btn btn-secondary" id="scheduleContinue">ต่อไป</button>
           </div>
+           
           <div class="tab-pane fade" id="reviewPanel" role="tabpanel">
             <h4>Review</h4>
+
+
+
+
+
+
+
+
+
+
+
+
+
             <button class="btn btn-primary btn-block" id="activate">Activate this Campaign!</button>
           </div>
+
+
+
+
+
+
+
+
         </div>
-        
+
      <!--
         <div class="progress mt-5">
           <div class="modalprogress-bar" role="progressbar" style="width: 20%" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100">Step 1 of 5</div>
@@ -390,8 +276,8 @@
 
       </div>
       <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
-        <button type="button" class="btn btn-primary">Save for later</button>
+        <button type="button" class="btn btn-primary">บันทึกผล</button>
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">ออก</button>
       </div>
     </div>
   </div>
