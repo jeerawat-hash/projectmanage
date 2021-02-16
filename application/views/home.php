4f770d54147ad6403ce6afa5a2005963d353c9a9
jeerawat <s6202041520016@email.kmutnb.ac.th>
2021-02-16 21:26:13 +0700

    add

--- application/views/home.php
+++ application/views/home.php
@@ -23,6 +23,17 @@
               <div class="card-header">
                 <h3 class="card-title">ความคืบหน้าโครงการ</h3>
               </div>
+              
+
+              <div class="card-tools">
+                <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
+                  <i class="fas fa-minus"></i>
+                </button>
+                <button type="button" class="btn btn-tool" data-card-widget="remove" title="Remove">
+                  <i class="fas fa-times"></i>
+                </button>
+              </div>
+
               <!-- /.card-header -->
               <div class="card-body">
                 
