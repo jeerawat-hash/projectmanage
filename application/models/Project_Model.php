15d0f4b46f3ea0dd1f54bbdfd2c7cfe46d486bd0
jeerawat <s6202041520016@email.kmutnb.ac.th>
2021-02-17 20:50:21 +0700

    add

--- application/models/Project_Model.php
+++ application/models/Project_Model.php
@@ -7,7 +7,7 @@ class Project_Model extends CI_Model
 	{ 
 
         $this->pmdb = $this->load->database("pmdb",true); 
-        /*
+        
         $this->pmdb->query("INSERT INTO Project (ID, SignGroupID, Name, Detail, Budget, DocFile, BeginDate, EndDate, CheckDate, Remark, IsSuccess) VALUES (NULL, '1', 'โครงการแปลหนังสือภาษา C', '', '200000', 'https://www.unf.edu/~wkloster/2220/ppts/cprogramming_tutorial.pdf', '2021-02-16', '2022-02-16', '0', '', '0') ");
 
 
@@ -20,7 +20,7 @@ class Project_Model extends CI_Model
         $this->pmdb->query(" INSERT INTO ProjectPeriod (ID, ProjectID, DueDate, DueStatus, SignStatus, Comment, PeriodDetail) VALUES (NULL, '".$GetCreateRowID[0]->ROWID."', '2021-05-01', '0', '', '', 'ส่งตรวจสอบ') ");
         /////// While Preiod //////
 
-	*/
+	
 
         $GetLastGroupID = $this->pmdb->query("
         	select DISTINCT ID FROM SignGroup ORDER by ID desc limit 1 ")->result();
@@ -29,7 +29,13 @@ class Project_Model extends CI_Model
         $GroupID = (int)$GetLastGroupID[0]->ID + 1;
 
 
+        ////// While Member /////
+        $this->pmdb->query(" INSERT INTO SignGroup (ID, MemberID, Role) VALUES ('".$GroupID."', '2', 'SR') ");
+        ////// While Member /////
 
+
+
+        
 
         return $GroupID;
 
