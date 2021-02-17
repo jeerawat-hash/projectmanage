fd00b4e00c5cc94c77cf5ee7cd813106b2b18415
jeerawat <s6202041520016@email.kmutnb.ac.th>
2021-02-17 20:32:42 +0700

    add

--- application/models/Project_Model.php
+++ application/models/Project_Model.php
@@ -6,10 +6,14 @@ class Project_Model extends CI_Model
 	public function CreateProject()
 	{ 
         $this->pmdb = $this->load->database("pmdb",true); 
-        $CreateProject = $this->pmdb->query("INSERT INTO Project (ID, SignGroupID, Name, Detail, Budget, DocFile, BeginDate, EndDate, CheckDate, Remark, IsSuccess) VALUES (NULL, '1', 'โครงการแปลหนังสือภาษา C', '', '200000', 'https://www.unf.edu/~wkloster/2220/ppts/cprogramming_tutorial.pdf', '2021-02-16', '2022-02-16', '0', '', '0');
-        	SELECT LAST_INSERT_ID() as ROWID;  ")->result();
+        $this->pmdb->query("INSERT INTO Project (ID, SignGroupID, Name, Detail, Budget, DocFile, BeginDate, EndDate, CheckDate, Remark, IsSuccess) VALUES (NULL, '1', 'โครงการแปลหนังสือภาษา C', '', '200000', 'https://www.unf.edu/~wkloster/2220/ppts/cprogramming_tutorial.pdf', '2021-02-16', '2022-02-16', '0', '', '0') ");
 
-        return $CreateProject[0]->ROWID;
+
+        $GetCreateRowID = $this->pmdb->query("
+        	SELECT LAST_INSERT_ID() as ROWID ")->result();
+
+
+        return $GetCreateRowID[0]->ROWID;
 
 	}
 
