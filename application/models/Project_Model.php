8f537c521961c9162a577a464d6db1a8650a06fb
jeerawat <s6202041520016@email.kmutnb.ac.th>
2021-02-17 20:40:50 +0700

    add

--- application/models/Project_Model.php
+++ application/models/Project_Model.php
@@ -5,7 +5,9 @@ class Project_Model extends CI_Model
  
 	public function CreateProject()
 	{ 
+
         $this->pmdb = $this->load->database("pmdb",true); 
+        /*
         $this->pmdb->query("INSERT INTO Project (ID, SignGroupID, Name, Detail, Budget, DocFile, BeginDate, EndDate, CheckDate, Remark, IsSuccess) VALUES (NULL, '1', 'โครงการแปลหนังสือภาษา C', '', '200000', 'https://www.unf.edu/~wkloster/2220/ppts/cprogramming_tutorial.pdf', '2021-02-16', '2022-02-16', '0', '', '0') ");
 
 
@@ -13,7 +15,24 @@ class Project_Model extends CI_Model
         	SELECT LAST_INSERT_ID() as ROWID ")->result();
 
 
-        return $GetCreateRowID[0]->ROWID;
+
+        /////// While Preiod //////
+        $this->pmdb->query(" INSERT INTO ProjectPeriod (ID, ProjectID, DueDate, DueStatus, SignStatus, Comment, PeriodDetail) VALUES (NULL, '".$GetCreateRowID[0]->ROWID."', '2021-05-01', '0', '', '', 'ส่งตรวจสอบ') ");
+        /////// While Preiod //////
+
+	*/
+
+        $GetLastGroupID = $this->pmdb->query("
+        	select DISTINCT ID FROM SignGroup ORDER by ID desc limit 1 ")->result();
+        
+
+        $GroupID = (int)$GetLastGroupID[0]->ID + 1;
+
+
+
+
+        return $GroupID;
+
 
 	}
 
