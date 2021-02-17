da2598fd885ea38b2fa58a444a7ad4812c8b19f1
jeerawat <s6202041520016@email.kmutnb.ac.th>
2021-02-17 18:02:44 +0700

    add

--- application/views/project.php
+++ application/views/project.php
@@ -546,9 +546,7 @@ $(".custom-file-input").on("change", function() {
                    url : "https://projectmanage.webclient.me/index.php/ProjectData/CreateProjectData",
                    type : "POST",
                    data : {
-                          A : data,
-                          B : Employee,
-                          C : PeriodInfo
+                           A: "s"
                           },
                    contentType : false,
                    cache : false,
