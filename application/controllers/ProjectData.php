c5a14aca027c533e428fb2fa6d6999211875c31c
jeerawat <s6202041520016@email.kmutnb.ac.th>
2021-02-17 20:28:32 +0700

    add

--- application/controllers/ProjectData.php
+++ application/controllers/ProjectData.php
@@ -19,15 +19,19 @@ class ProjectData extends CI_Controller {
 	{
 
 
-
+ 
 		print_r($_POST);  
 		print_r( json_decode($_POST["Employee"],true));  
 		print_r( json_decode($_POST["PeriodInfo"],true));   
 		print_r($_FILES);
+ 
+ 
+	}
+	public function test()
+	{
 
-
-
-
+		
+		echo $this->Project_Model->CreateProject();
 
 
 	}
--- application/models/Project_Model.php
+++ application/models/Project_Model.php
@@ -3,7 +3,15 @@ class Project_Model extends CI_Model
 { 
 
  
+	public function CreateProject()
+	{ 
+        $this->pmdb = $this->load->database("pmdb",true); 
+        $CreateProject = $this->pmdb->query("INSERT INTO Project (ID, SignGroupID, Name, Detail, Budget, DocFile, BeginDate, EndDate, CheckDate, Remark, IsSuccess) VALUES (NULL, '1', 'โครงการแปลหนังสือภาษา C', '', '200000', 'https://www.unf.edu/~wkloster/2220/ppts/cprogramming_tutorial.pdf', '2021-02-16', '2022-02-16', '0', '', '0');
+        	SELECT LAST_INSERT_ID() as ROWID;  ")->result();
 
+        return $CreateProject[0]->ROWID;
+
+	}
 
 
  
@@ -15,3 +23,4 @@ class Project_Model extends CI_Model
 }
 
  ?>
+
