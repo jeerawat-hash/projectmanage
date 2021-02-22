5288940d05b422cca6858a704304cfcf26aff50a
jeerawat <s6202041520016@email.kmutnb.ac.th>
2021-02-22 12:18:06 +0700

    add

--- application/views/projectdetail.php
+++ application/views/projectdetail.php
@@ -1,19 +1,4 @@
-
-<?php 
-
-  
-  print_r($ProjectINFO);
-  echo $ProjectINFO["ProjectInfo"][0]->Name;
-  echo $ProjectINFO["ProjectInfo"][0]->Detail;
-  echo $ProjectINFO["ProjectInfo"][0]->Budget;
-  echo $ProjectINFO["ProjectInfo"][0]->DocFile;
-  echo $ProjectINFO["ProjectInfo"][0]->CoutPreiod;
-
-
-  //echo $ProjectINFO["Period"];
-
-
- ?>
+ 
 
     <!-- Main content -->
     <section class="content">
