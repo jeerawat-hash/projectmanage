c51e0eaa987c1150e6154900b7d50137bc0282e1
p.jeerawat.th <s6202041520016@email.kmutnb.ac.th>
2021-04-06 22:52:57 +0700

    add

--- application/controllers/ProjectData.php
+++ application/controllers/ProjectData.php
@@ -199,7 +199,7 @@ class ProjectData extends CI_Controller {
 
 
 		SendMail_attach( "p.jeerawat.th@gmail.com", "Hello","TEST","jeerawat" );
-
+		notify("test","HxmtCfbCf8tSBgQIS2tJxpTOLYE6DFKJ4444shwaAWt");
 
 
 	}
@@ -250,3 +250,25 @@ function SendMail_attach( $ToEmail, $MessageHTML,$header,$Fromname ) {
 	}
   
   }
+
+
+  function notify($message,$token){
+
+	$lineapi = $token; 
+   $mms =  trim($message); 
+//   date_default_timezone_set("Asia/Bangkok");
+   $con = curl_init();
+   curl_setopt( $con, CURLOPT_URL, "https://notify-api.line.me/api/notify"); 
+   // SSL USE 
+   curl_setopt( $con, CURLOPT_SSL_VERIFYHOST, 0); 
+   curl_setopt( $con, CURLOPT_SSL_VERIFYPEER, 0); 
+   //POST 
+   curl_setopt( $con, CURLOPT_POST, 1); 
+   curl_setopt( $con, CURLOPT_POSTFIELDS, "message=$mms"); 
+   curl_setopt( $con, CURLOPT_FOLLOWLOCATION, 1); 
+   $headers = array( 'Content-type: application/x-www-form-urlencoded', 'Authorization: Bearer '.$lineapi.'', );
+	 curl_setopt($con, CURLOPT_HTTPHEADER, $headers); 
+   curl_setopt( $con, CURLOPT_RETURNTRANSFER, 1); 
+   $result = curl_exec( $con ); 
+
+}
