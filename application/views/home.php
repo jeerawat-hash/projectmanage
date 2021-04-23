cfd01a4c4ea6add640e1610868291e089294e568
p.jeerawat.th <s6202041520016@email.kmutnb.ac.th>
2021-04-23 14:42:51 +0700

    add

--- application/views/home.php
+++ application/views/home.php
@@ -5,6 +5,11 @@
         <div class="row">
           <div class="col-12">
 
+
+
+
+
+
           <div class="card card-success">
               <div class="card-header">
                 <h3 class="card-title">ผลรวมการดำเนินโครงการ</h3>
@@ -15,7 +20,207 @@
                 
                 <div class="row" id="GroupProgressProject">
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!--------------------- -->
+                <div class="col-sm-4 mt-4">
+                    <div class="position-relative p-3 bg-gray" style="height: 220px">
+                      <div class="ribbon-wrapper ribbon-lg">
+                        <div class="ribbon bg-success text-lg">
+                          ปกติ
+                        </div>
+                      </div>
+
+                      <div class="row">
+
+                        <div class="col-12"> 
+                                 [ชื่อโครงการ]
+                        </div> 
+                        <div class="col-12"> 
+                                 [รายละเอียดย่อ]
+                        </div> 
+
+                      </div>
+                      <hr>
+                      <div class="row">
+                        <div class="col-3 text-center"> 
+                                  <img alt="Avatar" class="table-avatar" style="width: 50%" src="https://blueprojectmanagement.com/assets/dist/img/avatar.png"> 
+                        </div>
+                        <div class="col-3 text-center"> 
+                                  <img alt="Avatar" class="table-avatar" style="width: 50%" src="https://blueprojectmanagement.com/assets/dist/img/avatar2.png"> 
+                        </div>
+                        <div class="col-3 text-center"> 
+                                  <img alt="Avatar" class="table-avatar" style="width: 50%" src="https://blueprojectmanagement.com/assets/dist/img/avatar3.png"> 
+                        </div>
+                        <div class="col-3 text-center"> 
+                                  <img alt="Avatar" class="table-avatar" style="width: 50%" src="https://blueprojectmanagement.com/assets/dist/img/avatar4.png"> 
+                        </div>
+                      </div>
+
+                      <br>
+
+                      <div class="row">
+                        <div class="col-12">
+                          
+
+                          <small> ดำเนินการแล้วเสร็จ </small>
+                          <div class="progress">
+                            <div class="progress-bar bg-success progress-bar-striped" role="progressbar"
+                                 aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 40%">
+                              <span class="sr-only">40% Complete </span>
+                            </div>
+                          </div>
+                          
+  
+                        </div>
+                      </div>
  
+                    </div>
+                  </div>
+
+
+
+                  <div class="col-sm-4 mt-4">
+                    <div class="position-relative p-3 bg-gray" style="height: 220px">
+                      <div class="ribbon-wrapper ribbon-xl">
+                        <div class="ribbon bg-warning text-lg">
+                          ใกล้ครบกำหนด
+                        </div>
+                      </div>
+                      <div class="row">
+
+                        <div class="col-12"> 
+                                 [ชื่อโครงการ]
+                        </div> 
+                        <div class="col-12"> 
+                                 [รายละเอียดย่อ]
+                        </div> 
+
+                      </div>
+                      <hr>
+                      <div class="row">
+                        <div class="col-3 text-center"> 
+                                  <img alt="Avatar" class="table-avatar" style="width: 50%" src="https://blueprojectmanagement.com/assets/dist/img/avatar.png"> 
+                        </div>
+                        <div class="col-3 text-center"> 
+                                  <img alt="Avatar" class="table-avatar" style="width: 50%" src="https://blueprojectmanagement.com/assets/dist/img/avatar2.png"> 
+                        </div>
+                        <div class="col-3 text-center"> 
+                                  <img alt="Avatar" class="table-avatar" style="width: 50%" src="https://blueprojectmanagement.com/assets/dist/img/avatar3.png"> 
+                        </div>
+                        <div class="col-3 text-center"> 
+                                  <img alt="Avatar" class="table-avatar" style="width: 50%" src="https://blueprojectmanagement.com/assets/dist/img/avatar4.png"> 
+                        </div>
+                      </div>
+
+                      <br>
+
+                      <div class="row">
+                        <div class="col-12">
+                          
+
+                          <small> ดำเนินการแล้วเสร็จ </small>
+                          <div class="progress">
+                            <div class="progress-bar bg-warning progress-bar-striped" role="progressbar"
+                                 aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 40%">
+                              <span class="sr-only">40% Complete </span>
+                            </div>
+                          </div>
+                          
+  
+                        </div>
+                      </div>
+
+                    </div>
+                  </div>
+
+
+
+
+
+
+                  <div class="col-sm-4 mt-4">
+                    <div class="position-relative p-3 bg-gray" style="height: 220px">
+                      <div class="ribbon-wrapper ribbon-xl">
+                        <div class="ribbon bg-danger text-lg">
+                          เกินกำหนด
+                        </div>
+                      </div>
+                      <div class="row">
+
+                        <div class="col-12"> 
+                                 [ชื่อโครงการ]
+                        </div> 
+                        <div class="col-12"> 
+                                 [รายละเอียดย่อ]
+                        </div> 
+
+                      </div>
+                      <hr>
+                      <div class="row">
+                        <div class="col-3 text-center"> 
+                                  <img alt="Avatar" class="table-avatar" style="width: 50%" src="https://blueprojectmanagement.com/assets/dist/img/avatar.png"> 
+                        </div>
+                        <div class="col-3 text-center"> 
+                                  <img alt="Avatar" class="table-avatar" style="width: 50%" src="https://blueprojectmanagement.com/assets/dist/img/avatar2.png"> 
+                        </div>
+                        <div class="col-3 text-center"> 
+                                  <img alt="Avatar" class="table-avatar" style="width: 50%" src="https://blueprojectmanagement.com/assets/dist/img/avatar3.png"> 
+                        </div>
+                        <div class="col-3 text-center"> 
+                                  <img alt="Avatar" class="table-avatar" style="width: 50%" src="https://blueprojectmanagement.com/assets/dist/img/avatar4.png"> 
+                        </div>
+                      </div>
+
+                      <br>
+
+                      <div class="row">
+                        <div class="col-12">
+                          
+
+                          <small> ดำเนินการแล้วเสร็จ </small>
+                          <div class="progress">
+                            <div class="progress-bar bg-danger progress-bar-striped" role="progressbar"
+                                 aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 40%">
+                              <span class="sr-only">40% Complete </span>
+                            </div>
+                          </div>
+                          
+  
+                        </div>
+                      </div>
+
+                    </div>
+                  </div>
+<!--------------------- -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
  
 
 
@@ -24,6 +229,8 @@
               <!-- /.card-body -->
             </div>
             <!-- /.card -->
+
+
 
  
 
@@ -37,8 +244,188 @@
                 
                 <div class="row" id="ProgressProject">
 
+
+
+
  
+
+
+
+<!--
+                  <div class="col-sm-4 mt-4">
+                    <div class="position-relative p-3 bg-gray" style="height: 220px">
+                      <div class="ribbon-wrapper ribbon-lg">
+                        <div class="ribbon bg-success text-lg">
+                          ปกติ
+                        </div>
+                      </div>
+
+                      <div class="row">
+
+                        <div class="col-12"> 
+                                 [ชื่อโครงการ]
+                        </div> 
+                        <div class="col-12"> 
+                                 [รายละเอียดย่อ]
+                        </div> 
+
+                      </div>
+                      <hr>
+                      <div class="row">
+                        <div class="col-3 text-center"> 
+                                  <img alt="Avatar" class="table-avatar" style="width: 50%" src="https://blueprojectmanagement.com/assets/dist/img/avatar.png"> 
+                        </div>
+                        <div class="col-3 text-center"> 
+                                  <img alt="Avatar" class="table-avatar" style="width: 50%" src="https://blueprojectmanagement.com/assets/dist/img/avatar2.png"> 
+                        </div>
+                        <div class="col-3 text-center"> 
+                                  <img alt="Avatar" class="table-avatar" style="width: 50%" src="https://blueprojectmanagement.com/assets/dist/img/avatar3.png"> 
+                        </div>
+                        <div class="col-3 text-center"> 
+                                  <img alt="Avatar" class="table-avatar" style="width: 50%" src="https://blueprojectmanagement.com/assets/dist/img/avatar4.png"> 
+                        </div>
+                      </div>
+
+                      <br>
+
+                      <div class="row">
+                        <div class="col-12">
+                          
+
+                          <small> ดำเนินการแล้วเสร็จ </small>
+                          <div class="progress">
+                            <div class="progress-bar bg-success progress-bar-striped" role="progressbar"
+                                 aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 40%">
+                              <span class="sr-only">40% Complete </span>
+                            </div>
+                          </div>
+                          
+  
+                        </div>
+                      </div>
  
+                    </div>
+                  </div>
+
+
+
+                  <div class="col-sm-4 mt-4">
+                    <div class="position-relative p-3 bg-gray" style="height: 220px">
+                      <div class="ribbon-wrapper ribbon-xl">
+                        <div class="ribbon bg-warning text-lg">
+                          ใกล้ครบกำหนด
+                        </div>
+                      </div>
+                      <div class="row">
+
+                        <div class="col-12"> 
+                                 [ชื่อโครงการ]
+                        </div> 
+                        <div class="col-12"> 
+                                 [รายละเอียดย่อ]
+                        </div> 
+
+                      </div>
+                      <hr>
+                      <div class="row">
+                        <div class="col-3 text-center"> 
+                                  <img alt="Avatar" class="table-avatar" style="width: 50%" src="https://blueprojectmanagement.com/assets/dist/img/avatar.png"> 
+                        </div>
+                        <div class="col-3 text-center"> 
+                                  <img alt="Avatar" class="table-avatar" style="width: 50%" src="https://blueprojectmanagement.com/assets/dist/img/avatar2.png"> 
+                        </div>
+                        <div class="col-3 text-center"> 
+                                  <img alt="Avatar" class="table-avatar" style="width: 50%" src="https://blueprojectmanagement.com/assets/dist/img/avatar3.png"> 
+                        </div>
+                        <div class="col-3 text-center"> 
+                                  <img alt="Avatar" class="table-avatar" style="width: 50%" src="https://blueprojectmanagement.com/assets/dist/img/avatar4.png"> 
+                        </div>
+                      </div>
+
+                      <br>
+
+                      <div class="row">
+                        <div class="col-12">
+                          
+
+                          <small> ดำเนินการแล้วเสร็จ </small>
+                          <div class="progress">
+                            <div class="progress-bar bg-warning progress-bar-striped" role="progressbar"
+                                 aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 40%">
+                              <span class="sr-only">40% Complete </span>
+                            </div>
+                          </div>
+                          
+  
+                        </div>
+                      </div>
+
+                    </div>
+                  </div>
+
+
+
+
+
+
+                  <div class="col-sm-4 mt-4">
+                    <div class="position-relative p-3 bg-gray" style="height: 220px">
+                      <div class="ribbon-wrapper ribbon-xl">
+                        <div class="ribbon bg-danger text-lg">
+                          เกินกำหนด
+                        </div>
+                      </div>
+                      <div class="row">
+
+                        <div class="col-12"> 
+                                 [ชื่อโครงการ]
+                        </div> 
+                        <div class="col-12"> 
+                                 [รายละเอียดย่อ]
+                        </div> 
+
+                      </div>
+                      <hr>
+                      <div class="row">
+                        <div class="col-3 text-center"> 
+                                  <img alt="Avatar" class="table-avatar" style="width: 50%" src="https://blueprojectmanagement.com/assets/dist/img/avatar.png"> 
+                        </div>
+                        <div class="col-3 text-center"> 
+                                  <img alt="Avatar" class="table-avatar" style="width: 50%" src="https://blueprojectmanagement.com/assets/dist/img/avatar2.png"> 
+                        </div>
+                        <div class="col-3 text-center"> 
+                                  <img alt="Avatar" class="table-avatar" style="width: 50%" src="https://blueprojectmanagement.com/assets/dist/img/avatar3.png"> 
+                        </div>
+                        <div class="col-3 text-center"> 
+                                  <img alt="Avatar" class="table-avatar" style="width: 50%" src="https://blueprojectmanagement.com/assets/dist/img/avatar4.png"> 
+                        </div>
+                      </div>
+
+                      <br>
+
+                      <div class="row">
+                        <div class="col-12">
+                          
+
+                          <small> ดำเนินการแล้วเสร็จ </small>
+                          <div class="progress">
+                            <div class="progress-bar bg-danger progress-bar-striped" role="progressbar"
+                                 aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 40%">
+                              <span class="sr-only">40% Complete </span>
+                            </div>
+                          </div>
+                          
+  
+                        </div>
+                      </div>
+
+                    </div>
+                  </div>
+-->
+
+                   
+                  
+
 
 
                 </div>
